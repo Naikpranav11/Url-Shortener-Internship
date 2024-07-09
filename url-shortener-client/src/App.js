302b11e84e22104ed2263f2957150ec23493b332
Naikpranav11 <lordpranav11@gmail.com>
2024-07-09 16:16:23 +0530

    jai shree ram

---
+++ url-shortener-client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UrlShortener from './UrlShortener';
+
+function App() {
+  return (
+    <div className="App">
+      <UrlShortener />
+    </div>
+  );
+}
+
+export default App;
